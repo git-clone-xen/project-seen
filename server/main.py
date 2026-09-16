e72da9e6f00e894ba0338cc54b30b40e1a471a36
Xen <75245762+git-clone-xen@users.noreply.github.com>
2026-09-17 00:52:41 +0800

    feat(connection): Log connects & welcome message

--- server/main.py
+++ server/main.py
@@ -1,7 +1,18 @@
 import socketio
-# socketio - handle live cconnections
+# socketio - handle live connections
 
 sio = socketio.AsyncServer(async_mode="asgi")
+
+# environ - dictionary containing standard HTTP and connection metadata
+#  about incoming client connection
+@sio.event
+async def connect(sid, environ):
+    print(f"someone connected: {sid}")
+    await sio.emit("welcome", {"msg": "welcome! u're connected"}, to=sid)
+
+@sio.event
+async def disconnect(sid, reason):
+    print(f"someone disconnected: {sid}")
 
 # visiting / directs user to index.html
 app = socketio.ASGIApp(sio, static_files={"/": "public/index.html"})
